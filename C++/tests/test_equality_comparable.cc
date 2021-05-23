87b8f5d9ed4048b22ea68d5b38d24c2e4ebc2aac
wddxrw <ruifengyue@gmail.com>
2021-05-23 06:27:59 +0000

    update .gitignore

--- C++/tests/test_equality_comparable.cc
+++ C++/tests/test_equality_comparable.cc
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
     Number n1(1);
     Number n2(2);
 
-    // 若不继承 boost::equality_comparable<Number> 这里的 != 会报错
+    // 若不继承 boost::equality_comparable<Number> 这里的 != 会报错 
     if (n1 != n2) {
         std::cout << "n1 == n2\n";
     }
